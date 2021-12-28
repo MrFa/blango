cb54482f3cdb1d43d994f3b0444100a9e1d68959
Fares Aliliche <faliliche@gmail.com>
2021-12-28 12:31:55 +0000

    Finish django configurations

--- blango/wsgi.py
+++ blango/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blango.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
+from configurations.wsgi import get_wsgi_application
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
 
 application = get_wsgi_application()
